1dc435938a43f44a508c4975ee919c7e9caf0090
Ryan Bryla <rmbryla@gmail.com>
2018-10-27 19:42:42 -0400

    crosshair image

---
+++ Assets/Scripts/Linear/GuiManager.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GuiManager : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void OnGUI()
+    {
+        GUI.Box(new Rect(0, 0, 170, 30), "Press E to spawn a shape");
+
+    }
+
+}
