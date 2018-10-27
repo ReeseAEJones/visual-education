c5ba7c2da380dc6ca81fa886be09bb703906d68f
ReeseAEJones <ReeseAEJones@Gmail.com>
2018-10-27 04:26:47 -0400

    Completed a basic manager class

---
+++ Assets/Scripts/Manager.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Manager : MonoBehaviour {
+
+    public Transform obj;
+
+	// Use this for initialization
+	void Start () {
+        Instantiate(obj, Vector3.zero, Quaternion.identity);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
