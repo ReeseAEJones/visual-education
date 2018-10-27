203fef026ca2de03ace3a081177528acf4edab7c
ReeseAEJones <ReeseAEJones@Gmail.com>
2018-10-27 08:53:13 -0400

    Completed main memu

---
+++ Assets/Scripts/TextButton.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TextButton : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        GetComponent<Renderer>().material.color = Color.black;
+	}
+
+    private void OnMouseEnter()
+    {
+        GetComponent<Renderer>().material.color = Color.red;
+    }
+
+    private void OnMouseExit()
+    {
+        GetComponent<Renderer>().material.color = Color.black;
+    }
+
+    void OnMouseUp()
+    {
+        Application.LoadLevel(1);
+    }
+}
