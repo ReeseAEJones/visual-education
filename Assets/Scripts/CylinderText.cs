e358ba97605d35cf8feb24c54ed91c99af818d7a
gihpuccini <gxp6079@g.rit.edu>
2018-10-27 11:22:25 -0400

    Cylinder attempt 2

---
+++ Assets/Scripts/CylinderText.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CylinderText : MonoBehaviour {
+
+    string text;
+    Transform cylinder;
+    // Use this for initialization
+    void Start()
+    {
+        this.cylinder = this.gameObject.transform;
+        this.text = cylinder.GetChild(0).GetComponent<TextMesh>().text;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 size = cylinder.GetChild(1).transform.localScale;
+        text = "Cylinder Description:";
+        text += "\nArea: " + (2 * ((Mathf.PI * size.x * size.x) + (Mathf.PI * size.x *size.y))).ToString() + "m^2";
+        text += "\nVolume : " + (Mathf.PI * size.x * size.x * size.y).ToString() + "m^3";
+        cylinder.GetChild(0).GetComponent<TextMesh>().text = text;
+    }
+}
