05c725dc6e3985c824f8b3f1a92848b1cadf84a5
gihpuccini <gxp6079@g.rit.edu>
2018-10-27 02:57:19 -0400

    Created changing collor method

---
+++ Assets/Scripts/ChangeColor.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangeColor : MonoBehaviour {
+
+	MMaterial objMaterial;
+    Color objColor;
+
+    // Use this for initialization
+    void Start()
+    {
+        this.objMaterial = GetComponent<Renderer>().material;
+        this.objColor = objMaterial.color;
+    }
+
+    void ChangeColor(Color color)
+    {
+        GetComponent<Renderer>().material.color = color;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
