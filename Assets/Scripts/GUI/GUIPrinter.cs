8849617e06bcbb7157c771242ec462bf23a5ad37
Ryan Bryla <rmbryla@gmail.com>
2018-10-27 20:03:44 -0400

    Added small dot to distinguish the center of the screen

--- Assets/Scripts/GUI/GUIPrinter.cs
+++ Assets/Scripts/GUI/GUIPrinter.cs
@@ -6,21 +6,31 @@ public class GUIPrinter : MonoBehaviour {
     private string text;
     public string Text{ set { this.text = value; }}
     private Rect rect;
-	// Use this for initialization
-	void Start () {
+    public Texture2D crosshair;
+    public Rect position;
+    // Use this for initialization
+    void Start () {
         rect = new Rect(5, 5, 200, 100);
         this.text = "Initial";
+        position = new Rect(Screen.width / 2, Screen.height / 2, 5, 5);
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
-	}
+        // position = new Rect((Screen.width - crosshair.width) / 2, (Screen.height - crosshair.height) / 2, crosshair.width, crosshair.height);
+    }
 
 
     private void OnGUI()
     {
         GUI.Box(rect, text);
+        GUI.Box(position, "");
+        // GUI.DrawTexture(position, crosshair);
+        /*
+        float xMin = (Screen.width / 2) - (crosshairImage.width / 2);
+        float yMin = (Screen.height / 2) - (crosshairImage.height / 2);
+        GUI.DrawTexture(new Rect(xMin, yMin, crosshairImage.width, crosshairImage.height), crosshairImage);
+        */
     }
 }
 
