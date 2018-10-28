d6f948cd6f96c7302bdb00a78ef9e4cb2715eadb
gihpuccini <gxp6079@g.rit.edu>
2018-10-28 03:14:19 -0400

    Added script to linear buttons

--- Assets/Scripts/Settings.cs
+++ Assets/Scripts/Settings.cs
@@ -9,12 +9,12 @@ public static class Settings {
     public const string volumeMode = "Linear";
     public const string noMode = "None";
 
-    public static Vector3 linearPos = new Vector3(1, 1, 1);
+    public static Vector3 linearPos = new Vector3(-89, (float)-1.2,(float)125.5287);
     public static Vector3 areaPos = new Vector3(1, 1, 1);
     public static Vector3 volumePos = new Vector3(1, 1, 1);
     public static Vector3 hubPos = new Vector3(0, 4, 0);
 
-    public static Vector3 linearObjPos = new Vector3(4, 1, 1);
+    public static Vector3 linearObjPos = new Vector3(-92, (float)-1.2, (float)125.5287);
     public static Vector3 areaObjPos = new Vector3(4, 1, 1);
     public static Vector3 volumeObjPos = new Vector3(4, 1, 1);
 }
